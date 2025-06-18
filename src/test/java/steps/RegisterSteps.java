bf7f6797f7c9baed7d666ea0793735a3681a0fcf
amos-maganyane <amos.maganyane@enviro365.co.za>
2025-06-18 19:32:23 +0200

    implement Then step for response status validation

--- src/test/java/steps/RegisterSteps.java
+++ src/test/java/steps/RegisterSteps.java
@@ -36,9 +36,9 @@ public class RegisterSteps {
 
     @Then("the response status code should be {int}")
     public void the_response_status_code_should_be(int statusCode) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        response.then().statusCode(statusCode);
     }
+
     @Then("the response should contain a user ID")
     public void the_response_should_contain_a_user_id() {
         // Write code here that turns the phrase above into concrete actions
