9a5c55c1f5cbd1842981f138b4beea3a5a2cd6c5
amos-maganyane <amos.maganyane@enviro365.co.za>
2025-06-18 15:29:33 +0200

    implement Cucumber test suite runner

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = "cucumber.glue", value = "steps")
+public class TestRunner {
+}
+
